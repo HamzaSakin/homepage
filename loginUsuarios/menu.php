2bd91ea4a6df69d05b14edf8c38219616a8c2c84
HamzaSakin <136713554+HamzaSakin@users.noreply.github.com>
2023-09-06 11:47:44 +0200

    Delete loginUsuarios directory

--- loginUsuarios/menu.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'utils.php';
-//require 'bbdd.php';
-require 'menuNavegador.php';
-require 'galeria.php';
-//require 'footer.inc'; ?>
-
-</body>
-</html>
--- loginUsuarios/utils.php
+++
@@ -1,7 +0,0 @@
-<?php
-define ("servername", "localhost");
-define ("username", "root");
-define ("password", "");
-define ("dataBase", "claseweb");
-
-//const servername = "localhost";
